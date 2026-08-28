d0e2be3524971791f07865b2e67832cbf89d5c89
Zevius27 <zeviusaplha@gmail.com>
2026-08-28 15:42:10 +0530

    Practice 28-08-2026--03:41

---
+++ C/zev.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char name[] = "Zevius";
+    printf("Hello %s\n",name);
+    return 0;
+}
---
+++ Java/zev.java
@@ -0,0 +1,6 @@
+public class zev {
+  public static void main(String[] args) {
+    String name = "Zevius";
+    System.out.println("Hi " + name);
+  }
+}
